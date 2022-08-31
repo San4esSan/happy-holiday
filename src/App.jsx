ce5578939f9a09aa36aa0e991fac7aa79eb2a770
mister.San4es <mister.San4es@list.ru>
2022-08-31 22:20:51 +0400

    day 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import Card from "./components/Card/Card";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import {ImgContextProvider} from "./context/imgContext";
+import {TextContextProvider} from "./context/textContext";
 
 const wrapper = {
   display: 'flex',
@@ -7,32 +10,16 @@ const wrapper = {
   height:'100vh',
 };
 
-const style = {};
-
 const App = () => {
   return (
     <div style={wrapper}>
-      <Header />
-      <div className={style.card}>
-        <div className={style.wrapper}>
-          <div className={style.image}>
-            <img className={style.img} src="" alt="" />
-            <p className={style.felicitation}>
-              Поздравляю с днем рожденья!
-              Пусть будет жизнь полна веселья,
-              Не будет грусти и хлопот,
-              А только счастье круглый год!
-
-              Желаю творческих успехов,
-              Прекрасных дней, улыбок, смеха.
-              Любви, душевного тепла,
-              Как сказка, чтобы жизнь была!
-
-            </p>
-          </div>
-        </div>
-      </div>
-      <Footer />
+      <ImgContextProvider>
+        <TextContextProvider>
+          <Header />
+          <Card />
+          <Footer />
+        </TextContextProvider>
+      </ImgContextProvider>
     </div>
   );
 }
